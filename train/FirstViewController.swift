3f291ea2ea8e2fa63f70f20356230a0ce561e4ff
ghost <t>
2018-09-10 10:34:54 +0800

    Initial Commit

---
+++ train/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  train
+//
+//  Created by Ghost on 2018/9/10.
+//  Copyright © 2018 Ghost. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
